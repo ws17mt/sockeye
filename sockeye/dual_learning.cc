c029142c0605f8d77c5d0ddeefd03381bd85c631
Vu Cong Duy Hoang <duyvuleo@gmail.com>
2017-07-02 11:19:02 -0400

    [dual_learning] delete .cc

--- sockeye/dual_learning.cc
+++
@@ -1,8 +0,0 @@
-# This is an implementation of the following work:
-# Dual Learning for Machine Translation
-# Yingce Xia, Di He, Tao Qin, Liwei Wang, Nenghai Yu, Tie-Yan Liu, Wei-Ying Ma
-# https: //arxiv.org/abs/1611.00179 (accepted at NIPS 2016)
-# Developed by Vu Cong Duy Hoang (vhoang2@student.unimelb.edu.au)
-# Date: July 2017
-# ---------------------------------------------------------------------------------
-
