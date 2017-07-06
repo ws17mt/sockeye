bd74a5d89d3e3c3874d0f023ccddfc4a66bb141f
Gaurav Kumar <gaurav.bison@gmail.com>
2017-07-06 14:26:44 -0400

    Iterators on mono datasets work well now.

--- sockeye/train_style.py
+++ sockeye/train_style.py
@@ -7,19 +7,20 @@ from train import _build_or_load_vocab
 
 logger = setup_main_logger(__name__, file_logging=False, console=True)
 
-source = "/export/b09/ws15gkumar/experiments/JSALT17/code/JSALT17-NMT-Lab/data/multi30k/train-toy.de.atok"
-target = "/export/b09/ws15gkumar/experiments/JSALT17/code/JSALT17-NMT-Lab/data/multi30k/train-toy.en.atok"
+source = "/Users/gaurav/Dropbox/Projects/JSALT17-NMT-Lab/data/multi30k/train-toy.de.atok"
+target = "/Users/gaurav/Dropbox/Projects/JSALT17-NMT-Lab/data/multi30k/train-toy.en.atok"
 
 num_words = 50000
 word_min_count = 1
 batch_size = 50
+max_seq_len=200
 
 # Build vocab
 vocab_source = _build_or_load_vocab(None, source, num_words, word_min_count)
 vocab_target = _build_or_load_vocab(None, target, num_words, word_min_count)
 
 vocab_source_size = len(vocab_source)
-vocab_target_size = len(target_source)
+vocab_target_size = len(vocab_target)
 logger.info("Vocabulary sizes: source=%d target=%d", vocab_source_size, vocab_target_size)
 
 data_info = sockeye.data_io.StyleDataInfo(os.path.abspath(source),
@@ -29,7 +30,7 @@ data_info = sockeye.data_io.StyleDataInfo(os.path.abspath(source),
 
 source_train_iter = sockeye.data_io.get_style_training_data_iters(
                         source=data_info.source,
-                        vocab_source=vocab_source,
+                        vocab=vocab_source,
                         batch_size=batch_size,
                         fill_up=True,
                         max_seq_len=max_seq_len,
@@ -39,7 +40,7 @@ source_train_iter = sockeye.data_io.get_style_training_data_iters(
 
 target_train_iter = sockeye.data_io.get_style_training_data_iters(
                         source=data_info.target,
-                        vocab_source=vocab_target,
+                        vocab=vocab_target,
                         batch_size=batch_size,
                         fill_up=True,
                         max_seq_len=max_seq_len,
