47c3032fac94a2952651992ec10dd580ffbfd916
dlewis777 <lefranchboi@gmail.com>
2017-07-10 11:30:07 -0400

    added training/train files for the language model

---
+++ sockeye/traininglm.py
@@ -0,0 +1,129 @@
+# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may not
+# use this file except in compliance with the License. A copy of the License
+# is located at
+#
+#     http://aws.amazon.com/apache2.0/
+# 
+# or in the "license" file accompanying this file. This file is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+"""
+Code for training
+"""
+import logging
+import os
+import pickle
+import random
+import shutil
+import time
+from typing import AnyStr, List, Optional
+
+import mxnet as mx
+import numpy as np
+
+import sockeye.callback
+import sockeye.checkpoint_decoder
+import sockeye.constants as C
+import sockeye.data_io
+import sockeye.inference
+import sockeye.loss
+import sockeye.lr_scheduler
+import sockeye.model
+import sockeye.utils
+import lm
+
+logger = logging.getLogger(__name__)
+
+class TrainingLModel(sockeye.training.TrainingModel):
+    """
+    Defines an Encoder/Decoder model (with attention).
+    RNN configuration (number of hidden units, number of layers, cell type)
+    is shared between encoder & decoder.
+    :param model_config: Configuration object holding details about the model.
+    :param context: The context(s) that MXNet will be run in (GPU(s)/CPU)
+    :param train_iter: The iterator over the training data.
+    :param fused: If True fused RNN cells will be used (should be slightly more efficient, but is only available
+            on GPUs).
+    :param bucketing: If True bucketing will be used, if False the computation graph will always be
+            unrolled to the full length.
+    :param lr_scheduler: The scheduler that lowers the learning rate during training.
+    :param rnn_forget_bias: Initial value of the RNN forget biases.
+    """
+
+    def __init__(self,
+                 model_config: sockeye.model.ModelConfig,
+                 context: List[mx.context.Context],
+                 train_iter: sockeye.data_io.ParallelBucketSentenceIter,
+                 fused: bool,
+                 bucketing: bool,
+                 lr_scheduler,
+                 rnn_forget_bias: float) -> None:
+        super().__init__(model_config)
+        self.context = context
+        self.lr_scheduler = lr_scheduler
+        self.bucketing = bucketing
+        self._build_model_components(self.config.max_seq_len, fused, rnn_forget_bias)
+        self.module = self._build_module(train_iter, self.config.max_seq_len)
+        self.training_monitor = None
+        #Initialize with Language Model
+
+    def _build_model_components(self, max_seq_len, fused, rnn_forget_bias):
+
+        self.lm = sockeye.lm.get_lm_from_options(self.config.num_embed_source,
+                                                 self.config.vocab_source_size,
+                                                 self.config.dropout,
+                                                 self.config.rnn_num_layers,
+                                                 self.config.rnn_num_hidden,
+                                                 self.config.rnn_cell_type,
+                                                 self.config.rnn_residual_connections,
+                                                 self.config.rnn_forget_bias)
+
+
+    def _build_module(self,
+                      train_iter: sockeye.data_io.ParallelBucketSentenceIter,
+                      max_seq_len: int):
+        """
+        Initializes model components, creates training symbol and module, and binds it.
+        """
+        source = mx.sym.Variable(C.SOURCE_NAME)
+        source_length = mx.sym.Variable(C.SOURCE_LENGTH_NAME)
+        labels = mx.sym.reshape(data=mx.sym.Variable(C.TARGET_LABEL_NAME), shape=(-1,))
+
+        loss = sockeye.loss.get_loss(self.config)
+
+        data_names = [x[0] for x in train_iter.provide_data]
+        label_names = [x[0] for x in train_iter.provide_label]
+
+        def sym_gen(seq_lens):
+            """
+            Returns a (grouped) loss symbol given source & target input lengths.
+            Also returns data and label names for the BucketingModule.
+            """
+            source_seq_len, target_seq_len = seq_lens
+
+            source_encoded = self.lm.encode(source, source_length, seq_len=source_seq_len)
+
+            outputs = loss.get_loss(logits, labels)
+
+            return mx.sym.Group(outputs), data_names, label_names
+
+        if self.bucketing:
+            logger.info("Using bucketing. Default max_seq_len=%s", train_iter.default_bucket_key)
+            return mx.mod.BucketingModule(sym_gen=sym_gen,
+                                          logger=logger,
+                                          default_bucket_key=train_iter.default_bucket_key,
+                                          context=self.context)
+        else:
+            logger.info("No bucketing. Unrolled to max_seq_len=%s", max_seq_len)
+            symbol, _, __ = sym_gen(train_iter.buckets[0])
+            return mx.mod.Module(symbol=symbol,
+                                 data_names=data_names,
+                                 label_names=label_names,
+                                 logger=logger,
+                                 context=self.context)
+
+    
