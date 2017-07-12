eb0b51aa4c1b39427544366e70bcfaf1e6595b46
Colin Cherry <colin.a.cherry@gmail.com>
2017-07-12 16:25:49 -0400

    Code review

--- sockeye/traininglm.py
+++ sockeye/traininglm.py
@@ -63,7 +63,9 @@ class TrainingLModel(sockeye.training.TrainingModel):
         self.bucketing = bucketing
         self._build_model_components(self.config.max_seq_len, fused, rnn_forget_bias)
         self.module = self._build_module(train_iter, self.config.max_seq_len)
-        self.training_monitor = None	
+        self.lm_source_module = None
+        self.lm_target_module = None
+        self.training_monitor = None
 
     def _build_model_components(self, max_seq_len, fused, rnn_forget_bias):
         self.lm = sockeye.lm.get_lm_from_options(self.config.num_embed_source,
@@ -74,9 +76,10 @@ class TrainingLModel(sockeye.training.TrainingModel):
                                                  self.config.rnn_cell_type,
                                                  self.config.rnn_residual_connections,
                                                  rnn_forget_bias)
-	
+
         self.rnn_cells = [self.lm.rnn]
         self.built = True
+
     def _build_module(self,
                       train_iter: sockeye.data_io.MonoBucketSentenceIter,
                       max_seq_len: int):
