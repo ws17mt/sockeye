4d4b93af89a237fb02e8537400cf89d0702685cc
beckdaniel <daniel.e.beck@gmail.com>
2017-07-24 17:46:14 -0400

    blah

--- sockeye/gcn.py
+++ sockeye/gcn.py
@@ -63,21 +63,15 @@ class GCNCell(object):
 
     def convolve(self, adj, inputs, seq_len):
         # inputs go through linear transformation
-        #print(inputs.debug_str())
-        #input_shape = inputs.infer_shape_partial()
-        #print(input_shape)
+        # annoyingly, MXNet does not have a batched version
+        # of FullyConnected so we need some reshaping
         reshaped = mx.symbol.reshape(inputs, (-3, -1))
         outputs = mx.symbol.FullyConnected(data=reshaped, weight=self._W,
                                            bias=self._b, num_hidden=self._num_hidden,
                                            name='%sFC'%self._prefix)
         outputs = mx.symbol.reshape(outputs, (-1, seq_len, self._num_hidden))
-        #print(outputs.debug_str())
-        # now they are convolved according to the adj matrix
-        #with mx.AttrScope(__layout__=C.BATCH_MAJOR):
-        #    outputs = mx.sym.swapaxes(data=outputs, dim1=0, dim2=1)                                   
+        # now they are convolved according to the adj matrix                                  
         outputs = mx.symbol.batch_dot(adj, outputs)
-        #with mx.AttrScope(__layout__=C.TIME_MAJOR):
-        #    outputs = mx.sym.swapaxes(data=outputs, dim1=0, dim2=1)
         # finally, we apply a non-linear transformation
         outputs = mx.symbol.Activation(outputs, act_type=self._activation)
         return outputs
